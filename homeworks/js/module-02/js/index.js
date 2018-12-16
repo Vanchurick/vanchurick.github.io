57125d70c9b6f07f59c576ceabad022c440415b2
Vanchurick <davane4ek@gmail.com>
2018-12-16 19:04:01 +0200

    homework 02

---
+++ homeworks/js/module-02/js/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+let userInput;
+const numbers = [];
+let total = 0;
+let counter = 0;
+let onlyNumber;
+
+do {
+        userInput = prompt('Введите число');
+        onlyNumber = Number(userInput);
+        if (isNaN(onlyNumber)) {
+          alert(`Было введено не число. Вы ввели "${userInput}". Попробуйте еще раз.`);     
+        } else (numbers.push(onlyNumber))
+        counter++;        
+    } while (userInput !== null);
+
+let minusNull = numbers.pop();
+
+for (let i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+
+if (total === 0 ) {
+    alert('Повторите попытку. Введите числа!')
+  } else {
+    alert(`Общая сумма чисел равна ${total}`)
+  }
+
