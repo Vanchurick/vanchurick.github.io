b82fab920c4b727a0485d14e562b21b845d98e15
Vanchurick <davane4ek@gmail.com>
2018-12-25 15:35:14 +0200

    hw 3

---
+++ homeworks/js/module-03/js/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+// const isLoginValid = function(login) {
+//   return login.length >= 4 && login.length <= 16;
+// };
+
+const isLoginValid = login => login.length >= 4 && login.length <= 16;
+
+// const isLoginUnique = function(allLogins, login) {
+//   return allLogins.includes(login);
+// };
+
+const isLoginUnique = (allLogins, login) => allLogins.includes(login);
+
+const addLogin = function(allLogins = logins, login) {
+  if (isLoginValid(login)) {
+    if (isLoginUnique(allLogins, login)) {
+      alert("Такой логин уже используется!");
+    } else {
+      allLogins.push(login);
+      alert("Логин успешно добавлен!");
+    }
+  } else {
+    alert("Ошибка! Логин должен быть от 4 до 16 символов");
+  }
+};
+
+do {
+  let userLogin = prompt("Enter login");
+  if (userLogin === null) {
+    alert('Отменено пользователем!');
+    break;
+  }
+  addLogin(logins, userLogin);
+} while (logins.length < 10);
+
+console.log(logins);
