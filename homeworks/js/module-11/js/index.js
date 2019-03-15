7e6146693032951498c4b99f1541c942da62cfbd
Vanchurick <davane4ek@gmail.com>
2019-03-15 16:11:59 +0200

    hw 111

--- homeworks/js/module-11/js/index.js
+++ homeworks/js/module-11/js/index.js
@@ -93,7 +93,7 @@ const laptops = [
 ];
 
 let form = document.querySelector(".js-form");
-let matches = document.querySelector('.matches')
+let matches = document.querySelector(".matches");
 
 let sizes = [...document.querySelectorAll('[name="size"]')];
 let colors = [...document.querySelectorAll('[name="color"]')];
@@ -102,24 +102,24 @@ let releaseDates = [...document.querySelectorAll('[name="release_date"]')];
 form.addEventListener("submit", filterProducts);
 
 let btnClear = document.querySelector('[type="reset"]');
-btnClear.addEventListener('click', resetForm);
+btnClear.addEventListener("click", resetForm);
 
 const filter = { size: [], color: [], release_date: [] };
 
-const templateCard = document.querySelector('#card-temlate').innerHTML.trim();
+const templateCard = document.querySelector("#card-temlate").innerHTML.trim();
 const temp = Handlebars.compile(templateCard);
-const galleryCard = document.querySelector('.gallery-cards');
-galleryCard.innerHTML = laptops.reduce((acc, el) => acc + temp(el), '');
+const galleryCard = document.querySelector(".gallery-cards");
+galleryCard.innerHTML = laptops.reduce((acc, el) => acc + temp(el), "");
 
 function filterProducts(e) {
   e.preventDefault();
   createFilter();
-  createHtmml(filterResult(laptops))
+  createHtmml(filterResult(laptops));
 }
 
-function resetForm () {
+function resetForm() {
   form.reset();
-  galleryCard.innerHTML = laptops.reduce((acc, el) => acc + temp(el), '');
+  galleryCard.innerHTML = laptops.reduce((acc, el) => acc + temp(el), "");
 }
 
 function createFilter() {
@@ -130,95 +130,33 @@ function createFilter() {
     .map(el => +el.value);
 }
 
-function filterResult (arr) {
-  let sizeFilterResult = arr.filter(el => filter.size.includes(el.size));
+function filterResult(arr) {
+  let a = arr.reduce((acc, el) => {
+    if (
+      filter.size.includes(el.size) &&
+      filter.color.includes(el.color) &&
+      filter.release_date.includes(el.release_date)
+    ) {
+      acc.push(el);
+    }
+    return acc;
+  }, []);
 
-  let colorFilterResult = sizeFilterResult.filter(el => filter.color.includes(el.color));
-  if(sizeFilterResult.length === 0) {
-    colorFilterResult = arr.filter(el => filter.color.includes(el.color));
-  }
-  let releaseFilterResult = colorFilterResult.filter(el => filter.release_date.includes(el.release_date))
-  if(colorFilterResult.length === 0) {
-  releaseFilterResult = sizeFilterResult.filter(el => filter.release_date.includes(el.release_date))
-  }
-  if(colorFilterResult.length === 0 && sizeFilterResult.length === 0) {
-    releaseFilterResult = arr.filter(el => filter.release_date.includes(el.release_date))
-  }
-  
-  let s = sizeFilterResult.length;
-  let c = colorFilterResult.length;
-  let r = releaseFilterResult.length;
-  
-  if(s === 0 && c !==0 && filter.release_date.length !== 0) return releaseFilterResult;
-  if(s !== 0 && c !==0 && r !== 0) return releaseFilterResult;
-  if(s !== 0 && c ===0 && r === 0) return sizeFilterResult;
-  if(s === 0 && c !==0 && r === 0) return colorFilterResult;
-  if(s === 0 && c ===0 && r !== 0) return releaseFilterResult;
-  if(s !== 0 && c !==0 && r === 0) return colorFilterResult;
-  if(s !== 0 && c ===0 && r !== 0) return releaseFilterResult;
-  if(s === 0 && c !==0 && r !== 0) return releaseFilterResult;
-  if(s === 0 && c ===0 && r === 0) return arr;
+  return a;
 }
 
-
-function createHtmml (obj) {
-  galleryCard.innerHTML = obj.reduce((acc, el) => acc + temp(el), '');
+function createHtmml(obj) {
+  if (
+    filter.size.length === 0 ||
+    filter.color.length === 0 ||
+    filter.release_date.length === 0
+  ) {
+    alert(
+      "Filter parameters shood be checked!"
+    );
+    return;
+  }
+  if (obj.length === 0) alert("No matches! Choose another filter parameters!");
+  galleryCard.innerHTML = obj.reduce((acc, el) => acc + temp(el), "");
   matches.textContent = `Matches: ${obj.length}`;
 }
-
-
-
-let filtredLaptops = laptops.filter((el) => {
-  if (filter.size.includes(el.size) && filter.color.includes(el.color) && filter.release_date.includes(el.release_date)){
-    return el;
-  };
-  if (filter.release_date.length === 0 && filter.color.length === 0 && filter.size.length === 0) {
-    return el;
-  };
-  if (filter.release_date.length === 0) {
-    if (filter.size.includes(el.size) && filter.color.includes(el.color)){
-      return el;
-    };
-    if (filter.color.length === 0) {
-      if (filter.size.includes(el.size)){
-        return el;
-      };
-    }
-    if (filter.size.length === 0) {
-      if (filter.color.includes(el.color)){
-        return el;
-      };
-    }
-  }
-  if (filter.size.length === 0) {
-    if (filter.release_date.includes(el.release_date) && filter.color.includes(el.color)){
-      return el;
-    };
-    if (filter.color.length === 0) {
-      if (filter.release_date.includes(el.release_date)){
-        return el;
-      };
-    }
-    if (filter.release_date.length === 0) {
-      if (filter.color.includes(el.color)){
-        return el;
-      };
-    }
-  }
-  if (filter.color.length === 0) {
-    if (filter.size.includes(el.size) && filter.release_date.includes(el.release_date)){
-      return el;
-    };
-    if (filter.size.length === 0) {
-      if (filter.release_date.includes(el.release_date)){
-        return el;
-      };
-    }
-    if (filter.release_date.length === 0) {
-      if (filter.size.includes(el.size)){
-        return el;
-      };
-    }
-  }
- 
-});
